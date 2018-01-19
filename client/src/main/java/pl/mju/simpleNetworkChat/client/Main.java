fd6d64ba9920224b28bbde4336d19be2813ef5c0
juszczakmarek <juszczak.marek@gmail.com>
2018-01-19 23:40:22 +0100

    chat-2 - Client: folder structure created

---
+++ client/src/main/java/pl/mju/simpleNetworkChat/client/Main.java
@@ -0,0 +1,9 @@
+package pl.mju.simpleNetworkChat.client;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Client dummy implementation");
+    }
+
+}
